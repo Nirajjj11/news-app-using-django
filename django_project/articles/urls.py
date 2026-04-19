1aa4d444e00d8c2e39c61bfa3d084c0baa414a69
Your Name <nirajkumar09160@gmail.com>
2026-04-20 00:39:26 +0530

    Creation done done

--- django_project/articles/urls.py
+++ django_project/articles/urls.py
@@ -4,11 +4,13 @@ from .views import (
       ArticleDeleteView,
       ArticleDetailView,
       ArticleUpdateView,
+      ArticleCreateView,
 )
 
 urlpatterns =[
       path("",ArticleListView.as_view(), name="article_list"),
       path("<int:pk>/",ArticleDetailView.as_view(), name="article_detail"),
       path("<int:pk>/delete",ArticleDeleteView.as_view(), name="article_delete"),
-      path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit")
+      path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
+      path("new/", ArticleCreateView.as_view(), name="article_new"),
 ]
