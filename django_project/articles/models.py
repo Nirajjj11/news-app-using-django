8659adaacb87b4205432ea09eaab603a705652ce
Your Name <nirajkumar09160@gmail.com>
2026-04-20 00:20:59 +0530

    Some views done

--- django_project/articles/models.py
+++ django_project/articles/models.py
@@ -15,4 +15,4 @@ class Article(models.Model):
             return self.title
 
       def get_absolute_url(self):
-            return reverse("article_details",kwargs={'pk':self.pk})
+            return reverse("article_detail",kwargs={'pk':self.pk})
