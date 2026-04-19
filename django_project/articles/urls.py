82f199ac09b11e5146fa420ac24fa38f2847cc3a
Your Name <nirajkumar09160@gmail.com>
2026-04-19 23:54:45 +0530

    Article views revised

--- django_project/articles/urls.py
+++ django_project/articles/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import (
+      ArticleListView,
+      ArticleDeleteView,
+      ArticleDetailView,
+      ArticleUpdateView,
+)
 
 urlpatterns =[
       path("",ArticleListView.as_view(), name="article_list"),
+      path("<int:pk>/",ArticleDetailView.as_view(), name="articles_details"),
+      path("<int:pk>/delete",ArticleDeleteView.as_view(), name="article_delete"),
+      path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit")
 ]
