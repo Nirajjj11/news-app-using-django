75bd6b42d5a1a80090c302a468fa094948896315
Your Name <nirajkumar09160@gmail.com>
2026-04-20 09:32:53 +0530

    Comment have to start

--- django_project/articles/admin.py
+++ django_project/articles/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from .models import Article,Comment
 
 # Register your models here.
+# class CommentInline(admin.StackedInline):
+#       model = Comment
+#       extra = 0
 
+class CommentInline(admin.TabularInline):
+      model = Comment
+      extra = 0
+      
 class ArticleAdmin(admin.ModelAdmin):
+      inlines = [ 
+            CommentInline,
+      ]
       list_display = [
             'title',
             'body',
