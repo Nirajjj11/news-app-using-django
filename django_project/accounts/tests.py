26f44a267d1610d86535a143601047fd38cc0b9d
Your Name <nirajkumar09160@gmail.com>
2026-05-04 21:05:16 +0530

    test

--- django_project/accounts/tests.py
+++ django_project/accounts/tests.py
@@ -30,7 +30,6 @@ class UsersManagersTests(TestCase):
             self.assertTrue(admin_user.is_active) 
             self.assertTrue(admin_user.is_staff) 
             self.assertTrue(admin_user.is_superuser)
-            
 
 class SignupPageTests(TestCase):  # new 
       def test_url_exists_at_correct_location_signupview(self): 
