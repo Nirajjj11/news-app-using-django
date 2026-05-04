d1f772b099dc9ac0b0c7609090124f84a64a270d
Your Name <nirajkumar09160@gmail.com>
2026-05-04 21:00:56 +0530

    loose the model

--- django_project/analysis/utils.py
+++ django_project/analysis/utils.py
@@ -3,9 +3,9 @@ from textblob import TextBlob
 def get_sentiment(text):
       polarity = TextBlob(text).sentiment.polarity
 
-      if polarity > 0.1:
+      if polarity > 0.05:
             return "Positive"
-      elif polarity < -0.1:
+      elif polarity < -0.05:
             return "Negative"
       else:
             return "Neutral"
